9f1226ccccf584b405e15c21ff811a2542f26f82
vilmavuorma <112632790+vilmavuorma@users.noreply.github.com>
2024-03-12 20:13:54 +0200

    Update Config.js
    Api keyt suojattu

--- firebase/Config.js
+++ firebase/Config.js
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore,collection,addDoc,serverTimestamp } from 'firebase/firestore'
 
 const firebaseConfig = {
-
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC__AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
